9d1e8ac85b0f1d0840fe6e79d2b9843aad29d5ed
18101-muhammad <muhammadabdullah_07@outlook.com>
2022-03-28 14:54:36 +0000

    add

--- chat/urls.py
+++ chat/urls.py
@@ -3,16 +3,14 @@ from django.urls import path
 from . import views
 
 
-rlpatterns = [
+urlpatterns = [
     path('', views.index, name='index'),
-    path('chat/', views.chat, name='chats'),
+    path('chat/', views.chat_view, name='chats'),
     path('chat/<int:sender>/<int:receiver>/', views.message_view, name='chat'),
     path('chat/verify/', views.verify, name='verify'),
-    path('register/', views.register_view, name='register'),
     path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
     path('api/messages/', views.message_list, name='message-list'),
     path('logout/', logout, {'next_page': 'index'}, name='logout'),
     path('register/', views.register_view, name='register'),
     path('send_email/', views.send_email, name='send_email'),
-     
 ]    
