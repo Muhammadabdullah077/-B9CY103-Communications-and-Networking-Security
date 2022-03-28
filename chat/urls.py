9bcd609239a3ed830ac9345bb8e917222de4b896
Muhammad Abdullah <56304160+18101-muhammad@users.noreply.github.com>
2022-03-28 17:13:39 +0600

    logout

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,7 @@ rlpatterns = [
     path('', views.index, name='index'),
     path('chat/', views.chat, name='chats'),
     path('register/', views.register_view, name='register'),
+    path('api/messages/<int:sender>/<int:receiver>/', views.message_list, name='message-detail'),
+    path('api/messages/', views.message_list, name='message-list'),
+    path('logout/', logout, {'next_page': 'index'}, name='logout'),
 ]    
