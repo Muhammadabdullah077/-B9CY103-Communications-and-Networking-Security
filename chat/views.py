add4547352b133a831f732ccbc48db7eb39dc8a9
Muhammad Abdullah <56304160+18101-muhammad@users.noreply.github.com>
2022-03-28 17:20:31 +0600

    importing signup and massage modles etc..

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from chat.models import Message
+from chat.forms import SignUpForm
+from chat.serializers import MessageSerializer
+from .models import Profile
+from django.core.mail import send_mail
+from django.http import HttpResponse, HttpResponseRedirect
+from ChatApp import settings
