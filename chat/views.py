93da1b2ae48c0541060cb87ddb09147c658711a4
Muhammad Abdullah <56304160+18101-muhammad@users.noreply.github.com>
2022-03-28 17:23:20 +0600

    foucation called

--- chat/views.py
+++ chat/views.py
@@ -11,3 +11,15 @@ from .models import Profile
 from django.core.mail import send_mail
 from django.http import HttpResponse, HttpResponseRedirect
 from ChatApp import settings
+
+ef send_email(sender):
+    import random
+    profile = Profile.objects.get(user=User.objects.get(email=sender.user.email))
+    code = str(random.random())
+    if code:
+        print()
+        subject = "Verification Email"
+        body = {
+            'subject': "Verification | DBS Chat System",
+            'message': 'verification code is: ' + str(code),
+        }
