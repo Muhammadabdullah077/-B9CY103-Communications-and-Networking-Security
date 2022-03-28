458df0262365ccd236b564d04712b2edbe0e03fd
Muhammad Abdullah <56304160+18101-muhammad@users.noreply.github.com>
2022-03-28 17:12:33 +0600

    url

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.views import logout
+from django.urls import path
+
 rlpatterns = [
     path('', views.index, name='index'),
     path('chat/', views.chat, name='chats'),
+    path('register/', views.register_view, name='register'),
 ]    
